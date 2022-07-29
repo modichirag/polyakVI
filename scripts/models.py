3a42419787f7979e0c8583782ebd0f6b5cf1da7a
modichirag <cmodi@flatironinstitute.org>
2022-07-29 17:13:24 -0400

    trasnform samples

--- scripts/models.py
+++ scripts/models.py
@@ -28,10 +28,11 @@ class PDB_5():
             stansamples.append(np.array([refdrawsdict[i][key] for i in range(len(refdrawsdict))]).flatten())
         #print(stanmodel.code('stan'))
         
-        ##Transfomrm
+        ##Transform
         self.D = 4
         self.offset = 5
         self.earn = tf.constant(np.log(data['earn']).astype(np.float32))
+        self.y = self.earn
         self.height = tf.constant(np.log(data['height']).astype(np.float32))
         self.male = tf.constant(np.array(data['male']).astype(np.float32))
         self.samples = np.array(stansamples).copy().astype(np.float32).T
@@ -42,16 +43,18 @@ class PDB_5():
         mu = w0 + w1*self.height + w2*self.male + self.offset
         #sigma = tf.exp(logsigma)
         sigma = tf.nn.softplus(logsigma)
-        ll = tfd.Normal(mu, sigma).log_prob(self.earn)
+        ll = tfd.Normal(mu, sigma).log_prob(self.y)
         return tf.reduce_sum(ll, axis=1)
 
-    def gensamples(self, qdist, n=1000):
-        samples = qdist.sample(n).numpy()
+    def transform_samples(self, samples):
         samples[:, 0] += self.offset
         samples[:, -1] = tf.nn.softplus(samples[:, -1])
         #samples[-1] = tf.exp(samples[-1])
         return samples
 
+    def gensamples(self, qdist, n=1000):
+        samples = qdist.sample(n).numpy()
+        return self.transform_samples(samples)
 
 
 class PDB_1():
@@ -69,13 +72,13 @@ class PDB_1():
             stansamples.append(np.array([refdrawsdict[i][key] for i in range(len(refdrawsdict))]).flatten())
         #print(stanmodel.code('stan'))
         
-        ##Transfomrm
+        ##Transform
         self.D = 5
         self.offset = 88
         self.hs = tf.constant(np.array(data['mom_hs'])- np.mean(data['mom_hs']), dtype=tf.float32)
         self.iq = tf.constant(np.array(data['mom_iq'])- np.mean(data['mom_iq']), dtype=tf.float32)
         self.inter = self.hs*self.iq
-        self.yv = tf.constant(np.array(data['kid_score'], dtype=np.float32))
+        self.y = tf.constant(np.array(data['kid_score'], dtype=np.float32))
         self.samples = np.array(stansamples).copy().astype(np.float32).T
 
     @tf.function
@@ -83,16 +86,18 @@ class PDB_1():
         w0, w1, w2, w3, logsigma = tf.split(w, self.D, 1)
         mu = w0 + w1*self.hs + w2*self.iq + w3*self.inter + self.offset
         sigma = tf.nn.softplus(logsigma)
-        ll = tfd.Normal(mu, sigma).log_prob(self.yv)
+        ll = tfd.Normal(mu, sigma).log_prob(self.y)
         return tf.reduce_sum(ll, axis=1)
 
 
-    def gensamples(self, qdist, n=1000):
-        samples = qdist.sample(n).numpy()
+    def transform_samples(self, samples):
         samples[:, -1] = tf.nn.softplus(samples[:, -1])
         samples[:, 0] += self.offset
         return samples
 
+    def gensamples(self, qdist, n=1000):
+        samples = qdist.sample(n).numpy()
+        return self.transform_samples(samples)
 
 
 
