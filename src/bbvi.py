c9d72e091be7ba61807f2625e9a223c15b47e122
modichirag <cmodi@flatironinstitute.org>
2022-10-10 12:02:50 -0400

    partial refactor for log_lik_and_grad

--- src/bbvi.py
+++ src/bbvi.py
@@ -1,10 +1,15 @@
+##Stan gradients are only implemented for bbvi_full. 
+##They are not yet implemented for bbvi_score and bbvi_path
 import numpy as np
 import tensorflow as tf 
 
 
 @tf.function
-def bbvi_score(model, log_likelihood, prior=False, nsamples=tf.constant(32)):   
-    sample = tf.stop_gradient((model.sample(nsamples)))
+def bbvi_score(model, log_likelihood, grad_log_likelihood=None, prior=False, nsamples=tf.constant(32)):   
+    if grad_log_likelihood is not None:
+        raise NotImplementedError
+
+    sample = tf.stop_gradient((model.sample(nsamples))) *1.
     #sample = (model.sample(nsamples))
     with tf.GradientTape(persistent=True) as tape:
         tape.watch(model.trainable_variables)
@@ -23,7 +28,10 @@ def bbvi_score(model, log_likelihood, prior=False, nsamples=tf.constant(32)):
 
 
 @tf.function
-def bbvi_path(model, log_likelihood, prior=False, nsamples=tf.constant(32)):   
+def bbvi_path(model, log_likelihood, grad_log_likelihood=None, prior=False, nsamples=tf.constant(32)):   
+    if grad_log_likelihood is not None:
+        raise NotImplementedError
+
     eps = tf.stop_gradient(model.noise.sample(nsamples))
     with tf.GradientTape(persistent=True) as tape:
         print("creating graph")
@@ -43,11 +51,12 @@ def bbvi_path(model, log_likelihood, prior=False, nsamples=tf.constant(32)):
 
 
 @tf.function
-def bbvi_elbo(model, log_likelihood, prior=False, nsamples=tf.constant(32)):   
+def _bbvi_elbo(model, log_likelihood, prior=False, nsamples=tf.constant(32)):   
+
     with tf.GradientTape(persistent=True) as tape:
         print("creating graph")
         tape.watch(model.trainable_variables)
-        sample = model.sample(nsamples) 
+        sample = model.sample(nsamples) *1.
         logl = log_likelihood(sample)
         if prior: 
             logp = model.log_prior(sample)
@@ -60,17 +69,55 @@ def bbvi_elbo(model, log_likelihood, prior=False, nsamples=tf.constant(32)):
     return tf.reduce_mean(elbo), gradients
 
 
+@tf.function
+def _bbvi_elbo_gradloglik(model, log_likelihood, grad_log_likelihood, prior=False, nsamples=tf.constant(32)):   
+    with tf.GradientTape(persistent=True) as tape:
+        #print("creating graph")
+        tape.watch(model.trainable_variables)
+        sample = model.sample(nsamples) *1.
+        #
+        if prior: 
+            logp = model.log_prior(sample)
+        else:
+            logp = 0 
+        logq = model.log_prob(sample)
+        lpq = logp - logq
+        neglpq = -1.* lpq
 
-def train(qdist, log_likelihood, prior=False, opt=None, lr=1e-3, mode='full', nsamples=32, niter=1001, nprint=None, verbose=True, callback=None):
+    #logl = log_likelihood(sample)
+    logl = tf.numpy_function(log_likelihood, [sample], tf.float32)
+    elbo = logl + logp - logq
+    negelbo = -1. * elbo
+
+    
+    gradients_lpq = tape.gradient(neglpq, model.trainable_variables)
+    #gradients_loglik = grad_log_likelihood(sample)
+    gradients_loglik = tf.numpy_function(grad_log_likelihood, [sample], tf.float32)
+    gradients_loglik = tape.gradient(sample, model.trainable_variables, gradients_loglik)
+    gradients = [gradients_lpq[i] - gradients_loglik[i] for i in range(len(gradients_lpq))]
+    return tf.reduce_mean(elbo), gradients
+
+
+
+def bbvi_elbo(model, log_likelihood, grad_log_likelihood=None, prior=False, nsamples=tf.constant(32)):   
+    
+    if grad_log_likelihood is None:
+        return _bbvi_elbo(model, log_likelihood, prior=prior, nsamples=nsamples)
+    else:
+        return _bbvi_elbo_gradloglik(model, log_likelihood, grad_log_likelihood, prior=prior, nsamples=nsamples)
+
+
+
+def train(qdist, log_likelihood, grad_log_likelihood=None, prior=False, opt=None, lr=1e-3, mode='full', nsamples=32, niter=1001, nprint=None, verbose=True, callback=None):
     
     if opt is None: opt = tf.keras.optimizers.Adam(learning_rate=lr)
     elbos, epss, losses = [], [], []
     if nprint is None: nprint = niter //10
     for epoch in range(niter):
         
-        if mode == 'full': elbo, grads = bbvi_elbo(qdist, log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
-        elif mode == 'score': elbo,  grads = bbvi_score(qdist, log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
-        elif mode == 'path': elbo, grads = bbvi_path(qdist, log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
+        if mode == 'full': elbo, grads = bbvi_elbo(qdist, log_likelihood, grad_log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
+        elif mode == 'score': elbo,  grads = bbvi_score(qdist, log_likelihood, grad_log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
+        elif mode == 'path': elbo, grads = bbvi_path(qdist, log_likelihood, grad_log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
         elbo = elbo.numpy()
         #
         if np.isnan(elbo):
--- src/polyakvi.py
+++ src/polyakvi.py
@@ -70,6 +70,64 @@ def polyakvi_score(model, log_likelihood, prior=False, nsamples=tf.constant(32))
 
 
 @tf.function
+def polyakvi_scorenorm_adv(model, log_likelihood, prior=None, nsamples=tf.constant(32)):   
+    '''Implement L_2norm[\grad_theta \log(q) - \log(p)] as the loss
+    '''
+    #sample = tf.stop_gradient(model.sample(nsamples))
+    sample = model.sample(nsamples) * 1.
+    sample1 = tf.stop_gradient(sample *1.+0.)
+    
+    with tf.GradientTape(persistent=True) as tape:
+        tape.watch(sample)
+        
+        with tf.GradientTape(persistent=True) as tape_in:
+            tape_in.watch(sample)
+
+            logl = log_likelihood(sample)
+            if prior: 
+                logpr = model.log_prior(sample)
+            else:
+                logpr = 0 
+            logp = logl + logpr
+            logq = model.log_prob(sample)
+            f = logq - logp 
+            f = tf.reduce_mean(f)
+            
+        grad_theta = tape_in.gradient(f, sample)
+        loss = tf.linalg.norm(grad_theta)
+        
+    gradients_adv = tape.gradient(loss, sample)
+    sample2 = tf.stop_gradient(sample + 1e-2*gradients_adv)
+
+    #Now do score gradient 
+    logl1 = tf.stop_gradient(log_likelihood(sample1))
+    if prior: 
+        logpr1 = model.log_prior(sample1)
+    else:
+        logpr1 = 0 
+    logp1 = logl1 + logpr1
+
+    logl2 = tf.stop_gradient(log_likelihood(sample2))
+    if prior: 
+        logpr2 = model.log_prior(sample2)
+    else:
+        logpr2 = 0 
+    logp2 = logl2 + logpr2
+
+    with tf.GradientTape(persistent=True) as tape:
+        tape.watch(model.trainable_variables)
+        logq1 = model.log_prob(sample1)
+        logq2 = model.log_prob(sample2)
+        f = (logq1 - logp1) - (logq2 - logp2)
+        f = tf.reduce_mean(f)
+        elbo =  -1. * tf.reduce_mean(logq1 - logp1)
+        
+    gradients = tape.gradient(f, model.trainable_variables)
+    return elbo, f, gradients
+
+
+
+@tf.function
 def polyakvi_scorenorm(model, log_likelihood, prior=None, nsamples=tf.constant(32)):   
     '''Implement L_2norm[\grad_theta \log(q) - \log(p)] as the loss
     '''
@@ -182,6 +240,7 @@ def train(qdist, log_likelihood, prior=False,  mode='full', nsamples=1, niter=10
         elif mode == 'score': elbo, loss, grads = polyakvi_score(qdist, log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
         elif mode == 'qonly': elbo, loss, grads = polyakvi_qgrad(qdist, log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
         elif mode == 'scorenorm': elbo, loss, grads = polyakvi_scorenorm(qdist, log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
+        elif mode == 'scorenorm_adv': elbo, loss, grads = polyakvi_scorenorm_adv(qdist, log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
         elif mode == 'fullgradnorm': elbo, loss, grads = polyakvi_fullgradnorm(qdist, log_likelihood, prior=prior, nsamples=tf.constant(nsamples))
         else:
             print("\nERROR : mode should be one of - full, score, qonly, scorenorm, fullgradnorm\n")
