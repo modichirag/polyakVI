ffc18c212f77a66734de4ec1087ae529632dc080
modichirag <cmodi@flatironinstitute.org>
2022-10-10 12:01:35 -0400

    interface stan models with tf bbvi

---
+++ scripts/fitq_pdb.py
@@ -0,0 +1,194 @@
+import numpy as np
+import sys, os
+import argparse
+#
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+#
+sys.path.append('../../normalizing_flows/src/')
+import flows
+
+sys.path.append('../src/')
+import bbvi, polyakvi, mapp, hmc
+import gaussianq
+import diagnostics as dg
+
+import pdbmodels
+#
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--nmodel', type=int, help='which PDB model')
+parser.add_argument('--alg', type=str, help='which algorithm to use')
+parser.add_argument('--mode', type=str, help='which mode of the alg to use')
+#arguments for flow
+parser.add_argument('--nlayers', type=int, default=5, help='number of layers, default=5')
+parser.add_argument('--nhidden', type=int, default=32, help='number of hddden params, default=32')
+parser.add_argument('--qmodel', type=str, default="frg", help='model, one of maf, nsf or mdn, default=maf')
+parser.add_argument('--seed', type=int, default=0, help='seed between 0-999, default=0')
+parser.add_argument('--niter', type=int, default=5001, help='number of iterations in training')
+parser.add_argument('--nsamples', type=int, default=32, help='batch size, default=32')
+#arguments for BBVI
+parser.add_argument('--lr', type=float, default=0.001, help='lr for bbvi')
+#arguments for PolyakVI
+parser.add_argument('--epsmax', type=float, default=0.001, help='maximum step size for polyakvi')
+parser.add_argument('--beta', type=float, default=0., help='momentum for polyakvi')
+parser.add_argument('--gamma', type=float, default=0.9, help='gamma factor for polyakvi')
+parser.add_argument('--llambda', type=float, default=1.0, help='lambda for slack polyakvi')
+parser.add_argument('--delta', type=float, default=0.1, help='delta for slack polyakvi')
+#parser.add_argument('--slack', type=int, default=0, help='slack polyakvi or regular polyakvi')
+#arguments for path name
+parser.add_argument('--suffix', type=str, default="", help='suffix, default=""')
+
+args = parser.parse_args()
+badmodels = [11, 33, 40, 51, 68, 69, 77]
+
+###Check first if its a valid model
+modelname = "PDB_%d"%args.nmodel
+#model, refsamples = pdbmodels.setup_pdb_model(1)
+model = pdbmodels.PDBModel_TF(args.nmodel)
+print(model)
+D = model.dims
+dg.corner(model.samples, savepath='//mnt/ceph/users/cmodi/polyakVI/%s/'%modelname)
+np.save('//mnt/ceph/users/cmodi/polyakVI/%s/samples'%modelname, model.samples) 
+
+###
+savepath = '//mnt/ceph/users/cmodi/polyakVI/%s/'%modelname
+savepath += '%s-%s-%s/'%(args.alg, args.mode, args.qmodel)
+if args.alg == 'bbvi': savepath += 'lr%0.e/'%args.lr
+elif args.alg == 'polyak': savepath += 'g%0.1f-b%.1f-em%.e/'%(args.gamma, args.beta, args.epsmax)
+elif args.alg == 'solyak': savepath += 'l%0.e-d%.ef/'%(args.llambda, args.delta)
+if args.suffix !="": savepath = savepath[:-1] + "-" + args.suffix  + '/'
+
+os.makedirs(savepath, exist_ok=True)
+print("\nsave in path %s"%savepath)
+suptitle = "PDB%d-"%args.nmodel + "-".join(savepath.split('/')[-3:-1])
+print('\nSuptitle : ', suptitle)
+
+
+######################
+np.random.seed(0)
+print("Which model to use?")
+
+### MAP esimtate for initialization
+# x0 = np.random.random(D).reshape(1, D).astype(np.float32) 
+# #x0[0, -1] = 1. #model.y.numpy().std()
+# print("Start at : ", x0)
+# x0 = tf.Variable(x0)
+# x0, losses = mapp.train(x0, model.log_likelihood_and_grad, niter=10001, lr=1e-3)
+# print("Raw Mapp value : ", x0)
+# print("True Mapp value : ", model.constrain_samples(x0))
+# print("Posterior mean : ", model.samples.mean(axis=0))
+
+## HMC sample for initialization
+x0 = model.samples[-1:].copy()
+print("Sample from HMC : ", x0)
+print("Posterior mean : ", model.samples.mean(axis=0))
+if args.nmodel in badmodels:
+    x0 = np.random.random(D).reshape(1, D).astype(np.float32)*0.
+else:
+    x0 = model.unconstrain_samples(x0).astype(np.float32)
+print("Raw value : ", x0)
+############
+### Setup VI
+print()
+
+scale = 1.
+if args.nmodel in badmodels: scale = 0.1
+#if args.qmodel == 'maf': qdist = flows.MAFFlow(D, nlayers=args.nlayers, mu=tf.constant(x0[0]), fitmean=True)e
+if args.qmodel == 'maf': qdist = flows.MAFFlow(D, nlayers=args.nlayers,  fitmean=False)
+elif args.qmodel == 'aff': qdist = flows.AffineFlow(D)
+elif args.qmodel == 'mfg': qdist = gaussianq.MF_Gaussian(D, mu=tf.constant(x0[0]), scale=scale)
+elif args.qmodel == 'frg': qdist = gaussianq.FR_Gaussian(D, mu=tf.constant(x0[0]), scale=scale)
+else: 
+    print('Variational model has to be one of maf, mfg or frg')
+print('log prob : ', qdist.log_prob(np.random.random(D).reshape(1, D).astype(np.float32)))
+
+
+
+# warmup = True
+# while warmup:
+#     print("\nWarmup")
+#     try:
+#         _, _ = bbvi.train(qdist, model.log_likelihood_and_grad, mode=args.mode, lr=args.lr, niter=100) 
+#         warmup = False
+#     except:
+#         if args.qmodel == 'mfg': 
+#             qdist = gaussianq.MF_Gaussian(D, mu=tf.constant(x0[0]), scale=qdist.scale/2.)
+#         elif args.qmodel == 'frg': 
+#             qdist = gaussianq.FR_Gaussian(D, mu=tf.constant(x0[0]), scale=qdist.scale/2.)
+#         else:
+#             raise NotImplementedError 
+#         print("Retry with qdist scale : ", qdist.scale)
+
+
+###########
+def callback(qdist, ls, i):
+    if args.alg == 'polyak' or args.alg == 'solyak':
+        dg.plot_polyak_losses(*ls, savepath, savename='losses_%04d'%i, suptitle=suptitle)
+    else:
+        dg.plot_bbvi_losses(*ls, savepath, savename='losses_%04d'%i, suptitle=suptitle)
+    qsamples = qdist.sample(1000)
+    qsamples = model.constrain_samples(qsamples).numpy()
+    dg.compare_hist(qsamples, model.samples, savepath=savepath, savename='hist_%04d'%i, suptitle=suptitle)
+
+
+print("Start VI")
+if args.alg == 'polyak' or args.alg == 'solyak':
+  slack = 0
+  if args.alg == 'solyak': slack = 1
+  qdist, losses, elbos, epss = polyakvi.train(qdist, model.log_likelihood_and_grad,
+                                              mode=args.mode, 
+                                              nsamples=args.nsamples, 
+                                              niter=args.niter, 
+                                              callback=callback, 
+                                              slack=slack, 
+                                              epsmax=args.epsmax, 
+                                              gamma=args.gamma, 
+                                              beta=args.beta, 
+                                              delta=args.delta,
+                                              llambda=args.llambda
+                                            )
+
+  dg.plot_polyak_losses(losses, elbos, epss, savepath, suptitle=suptitle)
+  np.save(savepath + 'losses', losses)
+  np.save(savepath + 'elbo', elbos)
+  np.save(savepath + 'eps', epss)
+
+elif args.alg == 'bbvi':
+    qdist, elbos = bbvi.train(qdist, model.log_likelihood_and_grad,
+                              mode=args.mode,
+                              nsamples=args.nsamples, 
+                              lr=args.lr, 
+                              niter=args.niter, 
+                              callback=callback)
+    #
+    dg.plot_bbvi_losses(elbos, savepath, suptitle=suptitle)
+    np.save(savepath + 'elbo', elbos)
+    
+elif args.alg == 'hmc':
+    qsamples, accepted, probs, counts = hmc.sample(model.loglik, grad_log_prob=None, 
+                                                   step_size=0.001, 
+                                                   Nleapfrog=100, 
+                                                   D=model.D, 
+                                                   nsamples=1000, 
+                                                   burnin=100,
+                                                   q0=x0, nchains=4)
+    #
+    qdist = None
+    print("Fraction accepted: ", accepted.sum()/accepted.size)
+    print(qsamples.shape)
+    qsamples = np.squeeze(qsamples).reshape(-1, model.D)
+    qsamples = model.transform_samples(qsamples)
+    
+
+if qdist is not None:
+    qsamples = qdist.sample(1000)
+    qsamples = model.constrain_samples(qsamples).numpy()
+
+np.save(savepath + 'samples', qsamples)
+dg.compare_hist(qsamples, model.samples, savepath=savepath, savename='hist', suptitle=suptitle)
+
+
---
+++ scripts/pdbmodels.py
@@ -0,0 +1,256 @@
+import numpy as np
+import sys, os
+import json
+import tensorflow as tf
+#import MCMC as mcmc
+
+from posteriordb import PosteriorDatabase
+
+##Setup your posterior DB environment
+PDBPATH = os.path.join('/mnt/home/cmodi/Research/Projects/posteriordb/posterior_database/')
+CMDSTAN = '/mnt/home/cmodi/Research/Projects/cmdstan/'
+BRIDGESTAN = '/mnt/home/cmodi/Research/Projects/bridgestan2/'
+MODELDIR = '../compiled_models/'
+
+sys.path.append(BRIDGESTAN)
+import PythonClient as pbs
+
+def get_pdb_model(model_n):
+
+    pdb = PosteriorDatabase(PDBPATH)
+    modelnames = pdb.posterior_names()
+    posterior = pdb.posterior(modelnames[model_n])
+    posname = posterior.name
+    print("Model name :", posname)
+
+    stanmodel, data = posterior.model, posterior.data.values()
+
+    refdrawsdict = posterior.reference_draws()
+    keys = refdrawsdict[0].keys()
+    stansamples = []
+    for key in keys:
+        stansamples.append(np.array([refdrawsdict[i][key] for i in range(len(refdrawsdict))]).flatten())
+    samples = np.array(stansamples).copy().astype(np.float32).T
+
+    return stanmodel, data, samples
+
+
+def setup_pdb_model(model_n):
+
+    stanmodel, data, samples = get_pdb_model(model_n)
+
+    #Save stan model code
+    modeldir = MODELDIR + '/PDB_%02d/'%model_n
+    os.makedirs(modeldir, exist_ok=True)
+    modpath = modeldir + 'PDB_%02d'%model_n
+    modpath = os.path.abspath(modpath)
+    print(modpath)
+    with open(modpath + '.stan', 'w') as f:
+        f.write(stanmodel.code('stan'))
+
+    #Save corresponding data
+    datapath = modpath + '.data.json'
+    with open(datapath, 'w') as f:
+        json.dump(data, f,)
+
+    #Save compiled shared object
+    sopath = modpath + '_model.so'
+
+    try:
+        if os.path.isfile(sopath):
+            print("PDB_%02d_model.so file exists"%model_n)
+        model = pbs.PyBridgeStan(sopath, datapath)
+    except Exception as e:
+        print(e)
+        cwd = os.getcwd()
+        print(cwd)
+        os.chdir(BRIDGESTAN)
+        os.system('make  %s'%sopath) 
+        os.chdir(cwd)
+        model = pbs.PyBridgeStan(sopath, datapath)
+    
+    return model, samples
+
+
+# class PDBModel_TF():
+    
+#     def __init__(self, model_n):
+
+#         self.model_n = model_n
+#         self.model, self.samples = setup_pdb_model(model_n)
+#         self.dims = self.model.dims()
+
+
+#     # @tf.custom_gradient
+#     # def log_likelihood_and_grad(self, q):
+        
+#     #     def np_loglik(x):
+#     #         lk, _ = self.model.log_density_gradient(np.float64(x))
+#     #         return np.float32(lk)
+
+#     #     def loglik(x):
+#     #         lk = tf.numpy_function(np_loglik, [tf.cast(x, tf.float64)], Tout=tf.float32)
+#     #         return lk
+
+#     #     def np_grad_loglik(x):
+#     #         _, lk_g = self.model.log_density_gradient(np.float64(x))
+#     #         return np.float32(lk_g)
+
+#     #     def grad_loglik(x):
+#     #         lk_g = tf.numpy_function(np_grad_loglik, [tf.cast(x, tf.float64)], Tout=tf.float32)
+#     #         return lk_g
+
+#     #     lk = []
+#     #     lk = tf.map_fn(loglik, q, fn_output_signature=tf.float32)
+        
+#     #     def grad(upstream):
+#     #         lk_g = []
+#     #         lk_g = tf.map_fn(grad_loglik, q, fn_output_signature=tf.float32)
+#     #         return tf.expand_dims(upstream, -1) * lk_g
+#     #     return lk, grad
+
+
+
+#     @tf.custom_gradient
+#     def log_likelihood_and_grad(self, q):
+        
+#         def np_loglik_and_grad(x):
+#             lk, lk_g = self.model.log_density_gradient(np.float64(x))
+#             return np.float32(lk), np.float32(lk_g)
+
+#         def loglik_and_grad(x):
+#             lk, lk_g = tf.numpy_function(np_loglik_and_grad, [tf.cast(x, tf.float64)], Tout=[tf.float32,tf.float32])
+#             return lk, lk_g
+
+#         lk, lk_g = tf.map_fn(loglik_and_grad, q, fn_output_signature=(tf.float32, tf.float32))
+        
+#         def grad(upstream):
+#             return tf.expand_dims(upstream, -1) * lk_g
+#         return lk, grad
+
+
+#     def log_likelihood(self, q):
+        
+#         if type(q) != np.ndarray:
+#             q = q.numpy().astype(np.float64)
+#             convert = True
+#         else: 
+#             q = q.astype(np.float64)
+#             convert = False
+
+#         if len(q.shape) > 1:
+#             lk = []
+#             for iq in q: 
+#                 lk.append(self.model.log_density(iq))
+#             lk = np.array(lk).astype(np.float32)
+#         else:
+#             lk = self.model.log_density(q)
+
+#         if convert: lk = tf.constant(lk, dtype=tf.float32)
+#         return lk
+
+
+#     def grad_log_likelihood(self, q):
+        
+#         if type(q) != np.ndarray:
+#             q = q.numpy().astype(np.float64)
+#             convert = True
+#         else: 
+#             q = q.astype(np.float64)
+#             convert = False
+
+#         if len(q.shape) > 1:
+#             lk = []
+#             for iq in q: 
+#                 lk.append(self.model.log_density_gradient(iq)[1].astype(np.float32)*1.)
+#             lk = np.array(lk)
+#         else:
+#             lk = self.model.log_density_gradient(q)[1]
+
+#         if convert: lk = tf.constant(lk, dtype=tf.float32)
+#         return lk
+
+#     def constrain_samples(self, q):
+        
+#         if type(q) != np.ndarray:
+#             q = q.numpy().astype(np.float64)
+#             convert = True
+#         else: 
+#             q = q.astype(np.float64)
+#             convert = False
+
+#         if len(q.shape) > 1:
+#             x = []
+#             for iq in q: 
+#                 x.append(self.model.param_constrain(iq)*1.)
+#             x = np.array(x)
+#         else:
+#             x = self.model.param_constrain(q)
+
+#         if convert: x = tf.constant(x, dtype=tf.float32)
+#         return x
+
+
+#     def unconstrain_samples(self, q):
+        
+#         if type(q) != np.ndarray:
+#             q = q.numpy().astype(np.float64)
+#             convert = True
+#         else: 
+#             q = q.astype(np.float64)
+#             convert = False
+
+#         if len(q.shape) > 1:
+#             x = []
+#             for iq in q: 
+#                 x.append(self.model.param_unconstrain(iq)*1.)
+#             x = np.array(x)
+#         else:
+#             x = self.model.param_unconstrain(q)
+
+#         if convert: x = tf.constant(x, dtype=tf.float32)
+#         return x
+
+
+
+
+if __name__=="__main__":
+
+    try: 
+        model_n = int(sys.argv[1])
+    except Exception as e:
+        print("Exception while parsing command line argument for model number : ", e)
+        print("\nRequires a model number as a command line input")
+        sys.exit()
+
+    print("Run for model number %d "%model_n)
+    
+    model, refsamples = setup_pdb_model(model_n)
+
+    #
+    D = model.dims()
+    q = np.random.normal(1, 0.3, D)
+
+    print()
+    print(q)
+    print("log_density and gradient of the model:")
+    print(model.log_density_gradient(q))
+    print(model.log_density_gradient(q, propto = 1, jacobian = 0))
+    print()
+
+    # print("Start sampling with HMC")
+    # stepsize = 0.001
+    # steps = 100
+    # metric_diag = np.ones(D)
+    # nsamples = 10
+    # #
+    # #SET random seed to replicate sampling
+    # np.random.seed(2)
+    # sampler = mcmc.HMCDiag(model, stepsize=stepsize, steps=steps, metric_diag=metric_diag)
+    # theta = np.empty([nsamples, model.dims()])
+    # for m in range(nsamples):
+    #     theta[m, :], _ = sampler.sample()
+
+    # print(theta)
+
+
