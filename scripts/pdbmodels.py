904be3856e026bb219ddd400c6e9240bd23c4857
modichirag <cmodi@flatironinstitute.org>
2022-10-18 15:14:47 -0400

    interface stan models with tf

--- scripts/pdbmodels.py
+++ scripts/pdbmodels.py
@@ -72,144 +72,144 @@ def setup_pdb_model(model_n):
     return model, samples
 
 
-# class PDBModel_TF():
+class PDBModel_TF():
     
-#     def __init__(self, model_n):
+    def __init__(self, model_n):
 
-#         self.model_n = model_n
-#         self.model, self.samples = setup_pdb_model(model_n)
-#         self.dims = self.model.dims()
+        self.model_n = model_n
+        self.model, self.samples = setup_pdb_model(model_n)
+        self.dims = self.model.dims()
 
 
-#     # @tf.custom_gradient
-#     # def log_likelihood_and_grad(self, q):
+    # @tf.custom_gradient
+    # def log_likelihood_and_grad(self, q):
         
-#     #     def np_loglik(x):
-#     #         lk, _ = self.model.log_density_gradient(np.float64(x))
-#     #         return np.float32(lk)
+    #     def np_loglik(x):
+    #         lk, _ = self.model.log_density_gradient(np.float64(x))
+    #         return np.float32(lk)
 
-#     #     def loglik(x):
-#     #         lk = tf.numpy_function(np_loglik, [tf.cast(x, tf.float64)], Tout=tf.float32)
-#     #         return lk
+    #     def loglik(x):
+    #         lk = tf.numpy_function(np_loglik, [tf.cast(x, tf.float64)], Tout=tf.float32)
+    #         return lk
 
-#     #     def np_grad_loglik(x):
-#     #         _, lk_g = self.model.log_density_gradient(np.float64(x))
-#     #         return np.float32(lk_g)
+    #     def np_grad_loglik(x):
+    #         _, lk_g = self.model.log_density_gradient(np.float64(x))
+    #         return np.float32(lk_g)
 
-#     #     def grad_loglik(x):
-#     #         lk_g = tf.numpy_function(np_grad_loglik, [tf.cast(x, tf.float64)], Tout=tf.float32)
-#     #         return lk_g
+    #     def grad_loglik(x):
+    #         lk_g = tf.numpy_function(np_grad_loglik, [tf.cast(x, tf.float64)], Tout=tf.float32)
+    #         return lk_g
 
-#     #     lk = []
-#     #     lk = tf.map_fn(loglik, q, fn_output_signature=tf.float32)
+    #     lk = []
+    #     lk = tf.map_fn(loglik, q, fn_output_signature=tf.float32)
         
-#     #     def grad(upstream):
-#     #         lk_g = []
-#     #         lk_g = tf.map_fn(grad_loglik, q, fn_output_signature=tf.float32)
-#     #         return tf.expand_dims(upstream, -1) * lk_g
-#     #     return lk, grad
+    #     def grad(upstream):
+    #         lk_g = []
+    #         lk_g = tf.map_fn(grad_loglik, q, fn_output_signature=tf.float32)
+    #         return tf.expand_dims(upstream, -1) * lk_g
+    #     return lk, grad
 
 
 
-#     @tf.custom_gradient
-#     def log_likelihood_and_grad(self, q):
+    @tf.custom_gradient
+    def log_likelihood_and_grad(self, q):
         
-#         def np_loglik_and_grad(x):
-#             lk, lk_g = self.model.log_density_gradient(np.float64(x))
-#             return np.float32(lk), np.float32(lk_g)
+        def np_loglik_and_grad(x):
+            lk, lk_g = self.model.log_density_gradient(np.float64(x))
+            return np.float32(lk), np.float32(lk_g)
 
-#         def loglik_and_grad(x):
-#             lk, lk_g = tf.numpy_function(np_loglik_and_grad, [tf.cast(x, tf.float64)], Tout=[tf.float32,tf.float32])
-#             return lk, lk_g
+        def loglik_and_grad(x):
+            lk, lk_g = tf.numpy_function(np_loglik_and_grad, [tf.cast(x, tf.float64)], Tout=[tf.float32,tf.float32])
+            return lk, lk_g
 
-#         lk, lk_g = tf.map_fn(loglik_and_grad, q, fn_output_signature=(tf.float32, tf.float32))
+        lk, lk_g = tf.map_fn(loglik_and_grad, q, fn_output_signature=(tf.float32, tf.float32))
         
-#         def grad(upstream):
-#             return tf.expand_dims(upstream, -1) * lk_g
-#         return lk, grad
+        def grad(upstream):
+            return tf.expand_dims(upstream, -1) * lk_g
+        return lk, grad
 
 
-#     def log_likelihood(self, q):
+    def log_likelihood(self, q):
         
-#         if type(q) != np.ndarray:
-#             q = q.numpy().astype(np.float64)
-#             convert = True
-#         else: 
-#             q = q.astype(np.float64)
-#             convert = False
+        if type(q) != np.ndarray:
+            q = q.numpy().astype(np.float64)
+            convert = True
+        else: 
+            q = q.astype(np.float64)
+            convert = False
 
-#         if len(q.shape) > 1:
-#             lk = []
-#             for iq in q: 
-#                 lk.append(self.model.log_density(iq))
-#             lk = np.array(lk).astype(np.float32)
-#         else:
-#             lk = self.model.log_density(q)
+        if len(q.shape) > 1:
+            lk = []
+            for iq in q: 
+                lk.append(self.model.log_density(iq))
+            lk = np.array(lk).astype(np.float32)
+        else:
+            lk = self.model.log_density(q)
 
-#         if convert: lk = tf.constant(lk, dtype=tf.float32)
-#         return lk
+        if convert: lk = tf.constant(lk, dtype=tf.float32)
+        return lk
 
 
-#     def grad_log_likelihood(self, q):
+    def grad_log_likelihood(self, q):
         
-#         if type(q) != np.ndarray:
-#             q = q.numpy().astype(np.float64)
-#             convert = True
-#         else: 
-#             q = q.astype(np.float64)
-#             convert = False
+        if type(q) != np.ndarray:
+            q = q.numpy().astype(np.float64)
+            convert = True
+        else: 
+            q = q.astype(np.float64)
+            convert = False
 
-#         if len(q.shape) > 1:
-#             lk = []
-#             for iq in q: 
-#                 lk.append(self.model.log_density_gradient(iq)[1].astype(np.float32)*1.)
-#             lk = np.array(lk)
-#         else:
-#             lk = self.model.log_density_gradient(q)[1]
+        if len(q.shape) > 1:
+            lk = []
+            for iq in q: 
+                lk.append(self.model.log_density_gradient(iq)[1].astype(np.float32)*1.)
+            lk = np.array(lk)
+        else:
+            lk = self.model.log_density_gradient(q)[1]
 
-#         if convert: lk = tf.constant(lk, dtype=tf.float32)
-#         return lk
+        if convert: lk = tf.constant(lk, dtype=tf.float32)
+        return lk
 
-#     def constrain_samples(self, q):
+    def constrain_samples(self, q):
         
-#         if type(q) != np.ndarray:
-#             q = q.numpy().astype(np.float64)
-#             convert = True
-#         else: 
-#             q = q.astype(np.float64)
-#             convert = False
+        if type(q) != np.ndarray:
+            q = q.numpy().astype(np.float64)
+            convert = True
+        else: 
+            q = q.astype(np.float64)
+            convert = False
 
-#         if len(q.shape) > 1:
-#             x = []
-#             for iq in q: 
-#                 x.append(self.model.param_constrain(iq)*1.)
-#             x = np.array(x)
-#         else:
-#             x = self.model.param_constrain(q)
+        if len(q.shape) > 1:
+            x = []
+            for iq in q: 
+                x.append(self.model.param_constrain(iq)*1.)
+            x = np.array(x)
+        else:
+            x = self.model.param_constrain(q)
 
-#         if convert: x = tf.constant(x, dtype=tf.float32)
-#         return x
+        if convert: x = tf.constant(x, dtype=tf.float32)
+        return x
 
 
-#     def unconstrain_samples(self, q):
+    def unconstrain_samples(self, q):
         
-#         if type(q) != np.ndarray:
-#             q = q.numpy().astype(np.float64)
-#             convert = True
-#         else: 
-#             q = q.astype(np.float64)
-#             convert = False
+        if type(q) != np.ndarray:
+            q = q.numpy().astype(np.float64)
+            convert = True
+        else: 
+            q = q.astype(np.float64)
+            convert = False
 
-#         if len(q.shape) > 1:
-#             x = []
-#             for iq in q: 
-#                 x.append(self.model.param_unconstrain(iq)*1.)
-#             x = np.array(x)
-#         else:
-#             x = self.model.param_unconstrain(q)
+        if len(q.shape) > 1:
+            x = []
+            for iq in q: 
+                x.append(self.model.param_unconstrain(iq)*1.)
+            x = np.array(x)
+        else:
+            x = self.model.param_unconstrain(q)
 
-#         if convert: x = tf.constant(x, dtype=tf.float32)
-#         return x
+        if convert: x = tf.constant(x, dtype=tf.float32)
+        return x
 
 
 
